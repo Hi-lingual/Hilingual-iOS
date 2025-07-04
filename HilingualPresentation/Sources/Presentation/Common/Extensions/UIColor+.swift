96b5547c0aeec4791cfb14b46ed567a1057013d2
hyeyeonie <shy020916@naver.com>
2025-07-05 01:58:12 +0900

    [add] 접근 쉬운 코드로 변경

--- HilingualPresentation/Sources/Presentation/Common/Extensions/UIColor+.swift
+++ HilingualPresentation/Sources/Presentation/Common/Extensions/UIColor+.swift
@@ -44,3 +44,34 @@ extension UIColor {
         return UIColor(named: color.rawValue, in: .module, compatibleWith: nil) ?? .clear
     }
 }
+
+extension UIColor {
+    static var hilingualBlack: UIColor { .color(.hilingualBlack) }
+    static var hilingualOrange: UIColor { .color(.hilingualOrange) }
+    static var hilingualBlue: UIColor { .color(.hilingualBlue) }
+    
+    static var noun_bg: UIColor { .color(.noun_bg) }
+    static var pronoun_bg: UIColor { .color(.pronoun_bg) }
+    static var adj_bg: UIColor { .color(.adj_bg) }
+    static var adverb_bg: UIColor { .color(.adverb_bg) }
+    static var preposition_bg: UIColor { .color(.preposition_bg) }
+    static var interjection_bg: UIColor { .color(.interjection_bg) }
+    static var pronoun_text: UIColor { .color(.pronoun_text) }
+    static var adj_text: UIColor { .color(.adj_text) }
+    static var adverb_text: UIColor { .color(.adverb_text) }
+    static var preposition_text: UIColor { .color(.preposition_text) }
+    
+    static var black: UIColor { .color(.black) }
+    static var gray850: UIColor { .color(.gray850) }
+    static var gray700: UIColor { .color(.gray700) }
+    static var gray500: UIColor { .color(.gray500) }
+    static var gray400: UIColor { .color(.gray400) }
+    static var gray300: UIColor { .color(.gray300) }
+    static var gray200: UIColor { .color(.gray200) }
+    static var gray100: UIColor { .color(.gray100) }
+    static var white: UIColor { .color(.white) }
+    static var dim: UIColor { .color(.dim) }
+    
+    static var alertRed: UIColor { .color(.alertRed) }
+    static var infoBlue: UIColor { .color(.infoBlue) }
+}
