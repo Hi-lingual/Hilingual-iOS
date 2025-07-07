e8b3f6c1f3ec0ed87af8aff4368b2f18dd21c28f
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2025-07-08 04:47:28 +0900

    [feat] 데이트피커 바텀시트 구현

---
+++ HilingualPresentation/Sources/Presentation/Common/Components/Calendar/MonthPickerViewController.swift
@@ -0,0 +1,65 @@
+//
+//  DatePicker.swift
+//  HilingualPresentation
+//
+//  Created by 조영서 on 7/8/25.
+//
+
+import UIKit
+import SnapKit
+
+final class MonthPickerViewController: UIViewController {
+
+    var onDateSelected: ((Date) -> Void)?
+    
+    private let datePicker = UIDatePicker()
+    private let applyButton = CTAButton(style: .TextButton("적용하기"), autoBackground: false)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupUI()
+        setupLayout()
+        setupActions()
+    }
+
+    private func setupUI() {
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.locale = Locale(identifier: "ko_KR")
+
+        if #available(iOS 17.4, *) {
+            datePicker.datePickerMode = .yearAndMonth
+        } else {
+            datePicker.datePickerMode = .date
+        }
+
+        view.addSubview(datePicker)
+        view.addSubview(applyButton)
+    }
+
+    private func setupLayout() {
+        datePicker.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(16)
+            $0.leading.trailing.equalToSuperview()
+        }
+        
+        applyButton.snp.makeConstraints {
+            $0.top.equalTo(datePicker.snp.bottom).offset(19)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().inset(16)
+        }
+    }
+
+    private func setupActions() {
+        applyButton.addTarget(self, action: #selector(applyButtonTapped), for: .touchUpInside)
+    }
+
+    @objc private func applyButtonTapped() {
+        onDateSelected?(datePicker.date)
+        dismiss(animated: true)
+    }
+}
+
+#Preview {
+    MonthPickerViewController()
+}
