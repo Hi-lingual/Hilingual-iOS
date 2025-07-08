fda03ec414328de9d7a99a0d809a4b9474a12a96
hyeonju <shj040201@naver.com>
2025-07-08 18:22:21 +0900

    [feat] loginView ui 구현

--- HilingualPresentation/Sources/Presentation/Login/LoginView.swift
+++ HilingualPresentation/Sources/Presentation/Login/LoginView.swift
@@ -5,27 +5,84 @@
 //  Created by 성현주 on 7/5/25.
 //
 
+import UIKit
+import SnapKit
 import AuthenticationServices
+
 
 final class LoginView: BaseUIView {
 
+    // MARK: - UI Components
+
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "img_logo_ios", in: .module, compatibleWith: nil)
+
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    private let characterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "img_login_ios", in: .module, compatibleWith: nil)
+        return imageView
+    }()
+
     let appleLoginButton = ASAuthorizationAppleIDButton()
-    
+
+
+    //TODO: - 컴포넌트로 변경
+    let privacyPolicyButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("개인정보처리방침", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .suit(.body_m_14)
+        return button
+    }()
+
+    // MARK: - Init
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupUI()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    private func setupUI() {
-        addSubview(appleLoginButton)
+    // MARK: - Setup
+
+    override func setUI() {
+        backgroundColor = .hilingualOrange
+        addSubviews(
+            logoImageView,
+            appleLoginButton,
+            characterImageView,
+            privacyPolicyButton
+        )
+    }
+
+    override func setLayout() {
+        logoImageView.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide.snp.top).offset(180)
+            $0.centerX.equalToSuperview()
+        }
+
         appleLoginButton.snp.makeConstraints {
-            $0.center.equalToSuperview()
-            $0.height.equalTo(50)
-            $0.width.equalTo(280)
+            $0.bottom.equalToSuperview().inset(120)
+            $0.centerX.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(58)
+        }
+        
+        characterImageView.snp.makeConstraints {
+            $0.bottom.equalTo(appleLoginButton.snp.top)
+            $0.centerX.equalToSuperview()
+        }
+
+        privacyPolicyButton.snp.makeConstraints {
+            $0.top.equalTo(appleLoginButton.snp.bottom).offset(16)
+            $0.centerX.equalToSuperview()
         }
     }
 }
