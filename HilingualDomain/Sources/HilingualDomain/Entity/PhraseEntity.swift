72b082f10845200705ec3f3db83ced18f0adda92
jinsoeun <rosejinse@naver.com>
2025-07-10 02:41:48 +0900

    [feat] Bookmark Domain 구현

---
+++ HilingualDomain/Sources/HilingualDomain/Entity/PhraseEntity.swift
@@ -0,0 +1,25 @@
+//
+//  PhraseEntity.swift
+//  HilingualDomain
+//
+//  Created by 진소은 on 7/9/25.
+//
+
+public struct PhraseEntity {
+    let code: Int
+    let data: PhraseData
+    let message: String
+    
+    struct PhraseData {
+        let phraseList: PhraseList
+    }
+    
+    struct PhraseList {
+        let phraseId: Int64
+        let phraseType: [String]
+        let phrase: String
+        let explanation: String
+        let reason: String
+        let isMarked: Bool
+    }
+}
