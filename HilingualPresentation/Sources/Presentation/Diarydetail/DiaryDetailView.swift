2caab8d9bb9fadaf707900ca8955c7fe4f246571
jinsoeun <rosejinse@naver.com>
2025-07-14 01:15:47 +0900

    [add] DiaryDetailView 생성

---
+++ HilingualPresentation/Sources/Presentation/Diarydetail/DiaryDetailView.swift
@@ -0,0 +1,14 @@
+//
+//  DiaryDetailView.swift
+//  HilingualPresentation
+//
+//  Created by 진소은 on 7/8/25.
+//
+
+import UIKit
+import SnapKit
+
+final class DiaryDetailView: BaseUIView {
+    
+    
+}
