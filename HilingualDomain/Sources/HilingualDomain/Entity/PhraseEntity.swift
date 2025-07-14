2b05f0e120008bc0cf2038a8fd6f5ebcfa3ed0e7
jinsoeun <rosejinse@naver.com>
2025-07-15 00:35:41 +0900

    [chore] Phrase 관련 struct 수정

--- HilingualDomain/Sources/HilingualDomain/Entity/PhraseEntity.swift
+++ HilingualDomain/Sources/HilingualDomain/Entity/PhraseEntity.swift
@@ -6,21 +6,10 @@
 //
 
 public struct PhraseEntity {
-    let code: Int
-    let data: PhraseData
-    let message: String
-    
-    public struct PhraseData {
-        let phraseList: PhraseList
-    }
- 
-    public struct PhraseList {
-        let phraseId: Int64
-        let phraseType: [String]
-        let phrase: String
-        let explanation: String
-        let reason: String
-        let isMarked: Bool
-    }
+    let phraseId: Int
+    let phraseType: [String]
+    let phrase: String
+    let explanation: String
+    let reason: String
+    let isMarked: Bool
 }
-
