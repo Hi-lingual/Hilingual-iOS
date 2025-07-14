39197fae3c7c610cd2e968b975412b600958c5ec
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2025-07-14 16:03:50 +0900

    [chore] 카드프리뷰 상단 공백 오류 수정

--- HilingualPresentation/Sources/Presentation/Common/Components/HomeView/CardPreview.swift
+++ HilingualPresentation/Sources/Presentation/Common/Components/HomeView/CardPreview.swift
@@ -80,6 +80,7 @@ final class CardPreview: UIView {
         }
 
         cardStack.snp.makeConstraints {
+            $0.top.equalTo(12)
             $0.leading.equalToSuperview().inset(16)
             $0.centerY.equalToSuperview()
             $0.height.equalTo(74)
@@ -90,6 +91,7 @@ final class CardPreview: UIView {
         }
         
         previewImage.snp.makeConstraints {
+            $0.top.equalTo(12)
             $0.trailing.equalToSuperview().inset(16)
             $0.centerY.equalToSuperview()
             $0.size.equalTo(74)
@@ -114,6 +116,7 @@ final class CardPreview: UIView {
             }
             
             previewImage.snp.remakeConstraints {
+                $0.top.equalTo(12)
                 $0.trailing.equalToSuperview()
                 $0.centerY.equalToSuperview()
                 $0.size.equalTo(0)
