9b36de7638d67757b4eed388c02b11b5c8631806
hyeonju <shj040201@naver.com>
2025-07-15 18:33:43 +0900

    [feat] 토큰 추가

--- HilingualNetwork/Sources/HilingualNetwork/Foundation/TargetType/BaseTargetType.swift
+++ HilingualNetwork/Sources/HilingualNetwork/Foundation/TargetType/BaseTargetType.swift
@@ -6,21 +6,27 @@
 //
 
 import Foundation
-
 import Moya
 
 public protocol BaseTargetType: TargetType {}
 
-extension BaseTargetType{
+extension BaseTargetType {
 
     public var baseURL: URL {
         return NetworkEnvironment.shared.baseURL
     }
-    
+
     public var headers: [String: String]? {
-        return [
+        var baseHeaders: [String: String] = [
             "Content-Type": "application/json"
         ]
+
+        let accessToken = UserDefaultHandler.accessToken
+        if !accessToken.isEmpty {
+            baseHeaders["Authorization"] = "Bearer \(accessToken)"
+        }
+
+        return baseHeaders
     }
 
     public var sampleData: Data {
