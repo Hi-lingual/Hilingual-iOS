fac7918fe07b5f779d71edf7cc6cdc259e21a656
jinsoeun <rosejinse@naver.com>
2025-07-16 21:46:40 +0900

    [chore] 코드 내 주석 제거

--- HilingualPresentation/Sources/Presentation/Diarydetail/DiaryDetailViewModel.swift
+++ HilingualPresentation/Sources/Presentation/Diarydetail/DiaryDetailViewModel.swift
@@ -9,22 +9,5 @@ import Foundation
 import Combine
 
 public final class DiaryDetailViewModel: BaseViewModel {
-//    
-//    // MARK: - Input / Output
-//    
-//    struct Input {
-//        let viewDidLoad: AnyPublisher<Void, Never>
-//        let setBookmarkButtonTapped: AnyPublisher<Bool, Never>
-//    }
-//    
-//    struct Output {
-//        let diaryDetail: AnyPublisher<, Never>
-//        let bookmarkResult: AnyPublisher<Bool, Never>
-//    }
-//    
-//    // MARK: - Properties
-//    
-//    private let useCase: DiaryDetailUseCase
-//    
-//    private let 
+
 }
