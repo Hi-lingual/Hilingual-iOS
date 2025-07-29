a68eb0d6c6eae91d728bb74d3fb1b4e0c854ccb8
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2025-07-29 22:33:11 +0900

    [chore] 데모데이 로직 복구

--- HilingualPresentation/Sources/Presentation/Common/Components/HomeView/SelectedInfo.swift
+++ HilingualPresentation/Sources/Presentation/Common/Components/HomeView/SelectedInfo.swift
@@ -15,6 +15,7 @@ final class SelectedInfo: UIView {
     }
 
     // MARK: - Callback
+    
     var onDiaryPreviewTapped: (() -> Void)?
 
     // MARK: - UI Components
@@ -163,13 +164,13 @@ final class SelectedInfo: UIView {
 
         iconView.isHidden = true
         timeLeftStack.isHidden = true
-        
-//        if selectedDay > today {
-//            notWrittenLabel.text = "작성불가"
-//            notWrittenLabel.textColor = .gray300
-//            diaryLockView.isHidden = false
-//            return
-//        }
+
+        if selectedDay > today {
+            notWrittenLabel.text = "작성불가"
+            notWrittenLabel.textColor = .gray300
+            diaryLockView.isHidden = false
+            return
+        }
 
         if let _ = diaryId {
             notWrittenLabel.text = "작성완료"
@@ -184,8 +185,7 @@ final class SelectedInfo: UIView {
             return
         }
 
-        //if remainingTime > 0, let topic = topicData {
-        if let topic = topicData {
+        if remainingTime > 0, let topic = topicData {
             notWrittenLabel.text = "미작성"
             notWrittenLabel.textColor = .gray300
             cardTopicView.isHidden = false
@@ -196,9 +196,9 @@ final class SelectedInfo: UIView {
             return
         }
 
-//        notWrittenLabel.text = "미작성"
-//        notWrittenLabel.textColor = .gray300
-//        emptyDiaryView.isHidden = false
+        notWrittenLabel.text = "미작성"
+        notWrittenLabel.textColor = .gray300
+        emptyDiaryView.isHidden = false
     }
 
     func setSelectedDate(_ date: Date) {
