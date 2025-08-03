ec685037149d630cc02584f8c1e1bf24f543fd50
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2025-08-04 05:38:39 +0900

    [chore] 캘린더 헤더뷰 분리

--- HilingualPresentation/Sources/Presentation/Home/HomeView.swift
+++ HilingualPresentation/Sources/Presentation/Home/HomeView.swift
@@ -15,7 +15,6 @@ final class HomeView: BaseUIView {
     // MARK: - UI Components
     
     internal let profileView = ProfileView()
-    internal let headerView = CalendarHeaderView()
     internal let calendarView = CalendarView()
     internal let selectedInfo = SelectedInfo()
     
@@ -45,7 +44,6 @@ final class HomeView: BaseUIView {
         
         addSubviews(
             profileView,
-            headerView,
             calendarView,
             spacer,
             divider,
@@ -64,13 +62,13 @@ final class HomeView: BaseUIView {
             $0.height.equalTo(46)
         }
         
-        headerView.snp.makeConstraints {
-            $0.top.equalTo(profileView.snp.bottom).offset(12)
-            $0.horizontalEdges.equalToSuperview()
-        }
+//        headerView.snp.makeConstraints {
+//            $0.top.equalTo(profileView.snp.bottom).offset(12)
+//            $0.horizontalEdges.equalToSuperview()
+//        }
         
         calendarView.snp.makeConstraints {
-            $0.top.equalTo(headerView.snp.bottom)
+            $0.top.equalTo(profileView.snp.bottom).offset(12)
             $0.horizontalEdges.equalToSuperview()
         }
         
@@ -100,20 +98,17 @@ final class HomeView: BaseUIView {
     
     // MARK: - Binding
     
-    // 캘린더 하나로 묶기
     private func bindCalendar() {
-        headerView.onMonthChanged = { [weak self] newDate in
-            self?.calendarView.reload(for: newDate)
-            
-            let calendar = Calendar.current
-            let year = calendar.component(.year, from: newDate)
-            let month = calendar.component(.month, from: newDate)
-            
-            self?.onMonthChanged?(year, month)
-        }
-        
         calendarView.onDateSelected = { [weak self] date in
             self?.selectedInfo.setSelectedDate(date)
+        }
+
+        calendarView.onMonthChanged = { [weak self] date in
+            guard let self else { return }
+            let calendar = Calendar.current
+            let year = calendar.component(.year, from: date)
+            let month = calendar.component(.month, from: date)
+            self.onMonthChanged?(year, month)
         }
     }
 }
