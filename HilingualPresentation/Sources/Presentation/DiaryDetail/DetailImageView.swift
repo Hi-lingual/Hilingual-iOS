8186e6c1a56f42491f01a335ffc1f2d60e290e73
jinsoeun <rosejinse@naver.com>
2025-08-11 20:57:56 +0900

    [chore] FullFrameImage 9:16으로 뜨도록 로직 수정

--- HilingualPresentation/Sources/Presentation/DiaryDetail/DetailImageView.swift
+++ HilingualPresentation/Sources/Presentation/DiaryDetail/DetailImageView.swift
@@ -13,17 +13,17 @@ final class DetailImageView: UIView {
     // MARK: - UI Components
     
     private let imageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFit
-        return imageView
+        let image = UIImageView()
+        image.image = UIImage(named: "img_load_fail_large_ios", in: .module, with: nil)
+        image.contentMode = .scaleAspectFit
+        return image
     }()
     
     private let button: UIButton = {
-        let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "ic_close_24_w_ios", in: .module, with: nil), for: .normal)
-        button.tintColor = .white
-        button.addTarget(self, action: #selector(close), for: .touchUpInside)
-        return button
+        let closeButton = UIButton(type: .system)
+        closeButton.setImage(UIImage(named: "ic_close_24_w_ios", in: .module, with: nil), for: .normal)
+        closeButton.tintColor = .white
+        return closeButton
     }()
     
     // MARK: - LifeCycle
@@ -32,31 +32,72 @@ final class DetailImageView: UIView {
         super.init(frame: .zero)
         imageView.image = image
         setUI()
+        applyLayout(for: imageView.image)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // MARK: - Setup Method
+    // MARK: - Setup
     
     private func setUI() {
         backgroundColor = .black
         addSubviews(imageView, button)
+        
         imageView.snp.makeConstraints {
             $0.center.equalToSuperview()
             $0.width.equalToSuperview()
+            $0.height.equalTo(imageView.snp.width).multipliedBy(16.0 / 9.0)
         }
         
         button.snp.makeConstraints {
-            $0.top.equalTo(safeAreaLayoutGuide)
+            $0.top.equalTo(safeAreaLayoutGuide).inset(12)
             $0.leading.equalToSuperview().inset(12)
         }
+        
+        button.addTarget(self, action: #selector(close), for: .touchUpInside)
     }
     
-    // MARK: - Public Methods
+    // MARK: - Layout Logic
+    
+    private func applyLayout(for image: UIImage?) {
+        let target: CGFloat = 9.0 / 16.0
+        let ratio: CGFloat = {
+            guard let img = image, img.size.height > 0 else { return target }
+            return img.size.width / img.size.height
+        }()
+
+        imageView.snp.remakeConstraints {
+            $0.leading.trailing.equalToSuperview()
+
+            if ratio < target {
+                imageView.contentMode = .scaleAspectFill
+                imageView.clipsToBounds = true
+                $0.height.equalTo(imageView.snp.width).multipliedBy(16.0 / 9.0)
+                $0.bottom.equalTo(safeAreaLayoutGuide)
+            } else {
+                imageView.contentMode = .scaleAspectFit
+                imageView.clipsToBounds = false
+                $0.height.equalTo(imageView.snp.width).multipliedBy(1.0 / ratio)
+                $0.centerY.equalToSuperview()
+            }
+        }
+
+        bringSubviewToFront(button)
+        setNeedsLayout()
+        layoutIfNeeded()
+    }
+
+    func update(image: UIImage?) {
+        guard let img = image else { return }
+        imageView.image = img
+        applyLayout(for: img)
+    }
+    
+    // MARK: - Actions
     
     @objc public func close() {
-        self.removeFromSuperview()
+        removeFromSuperview()
     }
 }
