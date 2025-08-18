9ebb087f05f1713d3adcb880c628d3878324887f
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2025-08-19 00:49:15 +0900

    [feat] 피드 뷰, VC, VM 파일 세팅

---
+++ HilingualPresentation/Sources/Presentation/Feed/FeedViewController.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewController.swift
+//  HilingualPresentation
+//
+//  Created by 조영서 on 8/18/25.
+//
+
+import Foundation
+
+public final class FeedViewController: BaseUIViewController<FeedViewModel> {
+    
+}
---
+++ HilingualPresentation/Sources/Presentation/Feed/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  HilingualPresentation
+//
+//  Created by 조영서 on 8/18/25.
+//
+
+import Combine
+import Foundation
+import HilingualDomain
+
+public final class FeedViewModel: BaseViewModel {
+}
