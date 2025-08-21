33da4907e2508d9f128ecaf80753a706cf2bc2e3
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2025-08-21 18:33:30 +0900

    [feat] 내 피드 프로필 구현

---
+++ HilingualPresentation/Sources/Presentation/FeedProfile/MyFeed/MyFeedProfileViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MyFeedProfileViewModel.swift
+//  HilingualPresentation
+//
+//  Created by 조영서 on 8/21/25.
+//
+
+import Combine
+import Foundation
+import HilingualDomain
+
+public final class MyFeedProfileViewModel: BaseViewModel {
+}
