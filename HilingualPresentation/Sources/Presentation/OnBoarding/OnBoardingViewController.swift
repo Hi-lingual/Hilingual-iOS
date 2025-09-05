20c839c128267d5f91ac83c3e647e49a2274c49b
hyeonju <shj040201@naver.com>
2025-09-06 01:40:56 +0900

    [feat] Onboarding 프로필 이미지 등록 modal 연결

--- HilingualPresentation/Sources/Presentation/OnBoarding/OnBoardingViewController.swift
+++ HilingualPresentation/Sources/Presentation/OnBoarding/OnBoardingViewController.swift
@@ -7,11 +7,12 @@
 
 import Foundation
 import Combine
+import PhotosUI
 
 public final class OnBoardingViewController: BaseUIViewController<OnBoardingViewModel> {
 
     // MARK: - Properties
-    
+
     private let onBoardingView = OnBoardingView()
     private let nicknameSubject = PassthroughSubject<String, Never>()
     private let startTappedSubject = PassthroughSubject<Bool, Never>()
@@ -32,26 +33,25 @@ public final class OnBoardingViewController: BaseUIViewController<OnBoardingView
 
         agreementModal.isHidden = true
         agreementModal.snp.makeConstraints { $0.edges.equalToSuperview() }
+
+        bindModalCallbacks()
     }
 
-    // MARK: - Navigation
+    // MARK: - Custom Methods
+
     public override func navigationType() -> NavigationType? {
         .titleOnly("프로필 작성")
     }
 
-    // MARK: - Bind
-    public override func bind(viewModel: OnBoardingViewModel) {
-        onBoardingView.nicknameTextField.textField.addTarget(
-            self,
-            action: #selector(nicknameDidChange),
-            for: .editingChanged
-        )
+    public override func addTarget() {
+        onBoardingView.profileImageView.editButton.addTarget(self, action: #selector(profileTapped), for: .touchUpInside)
+        onBoardingView.nicknameTextField.textField.addTarget(self, action: #selector(nicknameDidChange), for: .editingChanged)
+        onBoardingView.startButton.addTarget(self, action: #selector(showAgreementModal), for: .touchUpInside)
+    }
 
-        onBoardingView.startButton.addTarget(
-            self,
-            action: #selector(showAgreementModal),
-            for: .touchUpInside
-        )
+    // MARK: - Bind
+
+    public override func bind(viewModel: OnBoardingViewModel) {
 
         let input = OnBoardingViewModel.Input(
             nicknameChanged: nicknameSubject.eraseToAnyPublisher(),
@@ -84,15 +84,26 @@ public final class OnBoardingViewController: BaseUIViewController<OnBoardingView
             .store(in: &cancellables)
     }
 
+    // MARK: - Private Methods
+
+    private func bindModalCallbacks() {
+        onBoardingView.onSelectDefaultImage = { [weak self] in
+            self?.onBoardingView.profileImageView.profileImageView.image = UIImage(named: "img_profile_normal_ios")
+//            self?.viewModel?.selectedImageData = nil
+        }
+
+        onBoardingView.onSelectFromGallery = { [weak self] in
+            self?.presentImagePicker()
+        }
+    }
+
     // MARK: - Actions
 
-    @objc
-    private func nicknameDidChange() {
+    @objc private func nicknameDidChange() {
         nicknameSubject.send(onBoardingView.nicknameTextField.text)
     }
 
-    @objc
-    private func showAgreementModal() {
+    @objc private func showAgreementModal() {
         agreementModal.onStart = { [weak self] in
             guard let self else { return }
             let adAgree = self.agreementModal.isAdAgreeSelected
@@ -100,5 +111,45 @@ public final class OnBoardingViewController: BaseUIViewController<OnBoardingView
         }
         agreementModal.isHidden = false
         agreementModal.showAnimation()
+    }
+
+    @objc private func profileTapped() {
+        onBoardingView.modal.isHidden = false
+        onBoardingView.modal.showAnimation()
+    }
+}
+
+extension OnBoardingViewController: PHPickerViewControllerDelegate {
+
+    func presentImagePicker() {
+        var config = PHPickerConfiguration()
+        config.selectionLimit = 1
+        config.filter = .images
+
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+
+    public func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+
+        guard let itemProvider = results.first?.itemProvider,
+              itemProvider.canLoadObject(ofClass: UIImage.self)
+        else {
+            return
+        }
+
+        itemProvider.loadObject(ofClass: UIImage.self) { [weak self] object, _ in
+            guard let self, let image = object as? UIImage else { return }
+
+            DispatchQueue.main.async {
+                self.onBoardingView.profileImageView.profileImageView.image = image
+
+                if let data = image.jpegData(compressionQuality: 0.9) {
+//                    self.viewModel?.selectedImageData = data
+                }
+            }
+        }
     }
 }
