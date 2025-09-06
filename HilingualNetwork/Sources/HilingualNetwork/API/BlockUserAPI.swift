b4b64a93028450cf5080394aeda383f69cfad8d6
hyeonju <shj040201@naver.com>
2025-09-07 01:40:49 +0900

    [chore] 유저 차단 해제 api 연결

--- HilingualNetwork/Sources/HilingualNetwork/API/BlockUserAPI.swift
+++ HilingualNetwork/Sources/HilingualNetwork/API/BlockUserAPI.swift
@@ -10,15 +10,19 @@ import Moya
 public enum BlockUserAPI {
     case fetchBlockUserList
     case blockUser(userId: Int)
+    case unblockUser(userId: Int)
 }
 
 extension BlockUserAPI: BaseTargetType {
+
     public var path: String {
         switch self {
         case .fetchBlockUserList:
             return "/users/mypage/blocks"
         case .blockUser(let userId):
-            return "/users/\(userId)/block"
+            return "/users/block/\(userId)"
+        case .unblockUser(let userId):
+            return "/users/unblock/\(userId)"
         }
     }
 
@@ -27,7 +31,9 @@ extension BlockUserAPI: BaseTargetType {
         case .fetchBlockUserList:
             return .get
         case .blockUser:
-            return .post
+            return .put
+        case .unblockUser:
+            return .delete 
         }
     }
 
