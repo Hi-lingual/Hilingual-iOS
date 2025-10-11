c06b7d3d440da958c4e3cf33adbbd46cf3711ba8
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2025-10-11 14:15:25 +0900

    [chore] 스크롤뷰 제약 및 배경색 변경

--- HilingualPresentation/Sources/Presentation/Home/HomeView.swift
+++ HilingualPresentation/Sources/Presentation/Home/HomeView.swift
@@ -9,15 +9,21 @@ import UIKit
 import SnapKit
 
 final class HomeView: BaseUIView {
-    
+
     var onMonthChanged: ((Int, Int) -> Void)?
-    
+
     // MARK: - UI Components
-    
+
     private(set) var profileView = ProfileView()
     private(set) var calendarView = CalendarView()
     private(set) var selectedInfo = SelectedInfo()
-    
+
+    private let contentView : UIView = {
+        let contentView = UIView()
+        contentView.backgroundColor = .hilingualBlack
+        return contentView
+    }()
+
     private let spacer: UIView = {
         let view = UIView()
         view.backgroundColor = .white
@@ -29,28 +35,23 @@ final class HomeView: BaseUIView {
         view.backgroundColor = .gray100
         return view
     }()
-    
-    private let scrollView = UIScrollView()
-    private let contentView = UIView()
-    
-    private let bottomBackground: UIView = {
-        let view = UIView()
-        view.backgroundColor = .white
-        return view
+
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.backgroundColor = .white
+        scrollView.bounces = false
+        return scrollView
     }()
-    
+
     // MARK: - Custom Method
-    
+
     override func setUI() {
         backgroundColor = .hilingualBlack
-        
-        scrollView.showsVerticalScrollIndicator = false
-        scrollView.bounces = false
-        
-        addSubviews(bottomBackground, scrollView)
-        
+
+        addSubview(scrollView)
         scrollView.addSubview(contentView)
-        
+
         contentView.addSubviews(
             profileView,
             calendarView,
@@ -58,58 +59,57 @@ final class HomeView: BaseUIView {
             divider,
             selectedInfo
         )
-        
+
         bindCalendar()
+        
     }
     
     override func setLayout() {
-        
+
         scrollView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
+            $0.top.equalTo(safeAreaLayoutGuide)
+            $0.horizontalEdges.equalToSuperview()
+            $0.bottom.equalToSuperview()
         }
-        
+
         contentView.snp.makeConstraints {
             $0.edges.equalToSuperview()
-            $0.width.equalToSuperview()
+            $0.width.equalTo(scrollView.snp.width)
         }
-        
+
         profileView.snp.makeConstraints {
-            $0.top.equalTo(contentView.safeAreaLayoutGuide).inset(8)
+            $0.top.equalToSuperview().inset(8)
             $0.horizontalEdges.equalToSuperview()
             $0.height.equalTo(46)
         }
-        
+
         calendarView.snp.makeConstraints {
             $0.top.equalTo(profileView.snp.bottom).offset(12)
             $0.horizontalEdges.equalToSuperview()
         }
-        
+
         spacer.snp.makeConstraints {
             $0.top.equalTo(calendarView.snp.bottom)
             $0.horizontalEdges.equalToSuperview()
             $0.height.equalTo(28)
         }
-        
+
         divider.snp.makeConstraints {
             $0.top.equalTo(spacer.snp.bottom)
             $0.horizontalEdges.equalToSuperview()
             $0.height.equalTo(4)
         }
-        
+
         selectedInfo.snp.makeConstraints {
             $0.top.equalTo(divider.snp.bottom)
             $0.horizontalEdges.equalToSuperview()
-            $0.bottom.equalToSuperview().inset(30)
-        }
-        
-        bottomBackground.snp.makeConstraints {
-            $0.top.equalTo(divider.snp.bottom)
-            $0.leading.trailing.bottom.equalToSuperview()
+            $0.bottom.equalToSuperview()
+
         }
     }
-    
+
     // MARK: - Binding
-    
+
     private func bindCalendar() {
         calendarView.onDateSelected = { [weak self] date in
             self?.selectedInfo.setSelectedDate(date)
