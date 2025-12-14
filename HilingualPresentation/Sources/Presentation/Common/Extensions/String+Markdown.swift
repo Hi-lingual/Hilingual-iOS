c444e33213d684b67288d60fe283eeaaeac7618d
hyeonju <shj040201@naver.com>
2025-12-14 23:29:43 +0900

    [fix] 마크다운 파서 수정 + 스크롤뷰 추가

--- HilingualPresentation/Sources/Presentation/Common/Extensions/String+Markdown.swift
+++ HilingualPresentation/Sources/Presentation/Common/Extensions/String+Markdown.swift
@@ -8,30 +8,94 @@
 import UIKit
 
 extension String {
-    func toMarkdownAttributedString(font: UIFont = .pretendard(.body_r_16),
-                                     textColor: UIColor = .gray850) -> NSAttributedString {
-        let components = self.components(separatedBy: "\n\n")
+    func toMarkdownAttributedString(
+        font: UIFont = .pretendard(.body_r_16),
+        textColor: UIColor = .gray850
+    ) -> NSAttributedString {
+        let paragraphs = components(separatedBy: "\n\n")
         let result = NSMutableAttributedString()
 
-        for (index, block) in components.enumerated() {
-            if let attr = try? AttributedString(markdown: block, options: .init(interpretedSyntax: .full)) {
-                var mutableAttr = NSMutableAttributedString(attributedString: NSAttributedString(attr))
-                mutableAttr.addAttributes([
-                    .font: font,
-                    .foregroundColor: textColor
-                ], range: NSRange(location: 0, length: mutableAttr.length))
+        for (index, paragraph) in paragraphs.enumerated() {
+            let processedParagraph = escapeNumberedList(in: paragraph)
+            let markdownParagraph = convertLineBreaks(in: processedParagraph)
 
-                result.append(mutableAttr)
+            if let attributedString = parseMarkdown(markdownParagraph, font: font, textColor: textColor) {
+                result.append(attributedString)
             } else {
-                result.append(NSAttributedString(string: block))
+                result.append(createPlainText(paragraph, font: font, textColor: textColor))
             }
 
-            // 문단 줄바꿈
-            if index < components.count - 1 {
+            if index < paragraphs.count - 1 {
                 result.append(NSAttributedString(string: "\n\n"))
             }
         }
 
         return result
+    }
+
+    private func escapeNumberedList(in text: String) -> String {
+        let pattern = "^(\\d+)\\. "
+        guard let regex = try? NSRegularExpression(pattern: pattern, options: .anchorsMatchLines) else {
+            return text
+        }
+
+        let range = NSRange(text.startIndex..., in: text)
+        return regex.stringByReplacingMatches(
+            in: text,
+            options: [],
+            range: range,
+            withTemplate: "$1\\\\. "
+        )
+    }
+
+    private func convertLineBreaks(in text: String) -> String {
+        text.replacingOccurrences(of: "\n", with: "  \n")
+    }
+
+    private func parseMarkdown(
+        _ text: String,
+        font: UIFont,
+        textColor: UIColor
+    ) -> NSAttributedString? {
+        guard let attributedString = try? AttributedString(
+            markdown: text,
+            options: .init(interpretedSyntax: .full)
+        ) else {
+            return nil
+        }
+
+        let mutableAttributedString = NSMutableAttributedString(attributedString: NSAttributedString(attributedString))
+        let paragraphStyle = createParagraphStyle()
+
+        mutableAttributedString.addAttributes(
+            [
+                .font: font,
+                .foregroundColor: textColor,
+                .paragraphStyle: paragraphStyle
+            ],
+            range: NSRange(location: 0, length: mutableAttributedString.length)
+        )
+
+        return mutableAttributedString
+    }
+
+    private func createParagraphStyle() -> NSParagraphStyle {
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = 4
+        return style
+    }
+
+    private func createPlainText(
+        _ text: String,
+        font: UIFont,
+        textColor: UIColor
+    ) -> NSAttributedString {
+        NSAttributedString(
+            string: text,
+            attributes: [
+                .font: font,
+                .foregroundColor: textColor
+            ]
+        )
     }
 }
