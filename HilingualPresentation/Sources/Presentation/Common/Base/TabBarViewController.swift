4d2a135edd0d7f6b0427ffec54b6aed9c1fd5e73
hyeonju <shj040201@naver.com>
2026-03-02 04:47:18 +0900

    [feat] 커스텀 탭바 추가

--- HilingualPresentation/Sources/Presentation/Common/Base/TabBarViewController.swift
+++ HilingualPresentation/Sources/Presentation/Common/Base/TabBarViewController.swift
@@ -6,125 +6,213 @@
 //
 
 import UIKit
+import SnapKit
 
-public final class TabBarViewController: UITabBarController {
-    
+public final class TabBarViewController: UIViewController {
+
     // MARK: - Dependencies
-    
+
     private let factory: ViewControllerFactory
-    
-    // MARK: - Init
-    
-    public init(diContainer: ViewControllerFactory) {
-        self.factory = diContainer
-        super.init(nibName: nil, bundle: nil)
+    private let customTabBarHeight: CGFloat = 50
+
+    // MARK: - Child ViewControllers
+
+    private var childNavigationControllers: [UINavigationController] = []
+    private var currentIndex: Int = 0
+
+    public var selectedIndex: Int {
+        get { currentIndex }
+        set { selectTab(at: newValue) }
     }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    // MARK: - LifeCycle
-    
-    public override func viewDidLoad() {
-        super.viewDidLoad()
-        delegate = self
-        setupTabBarAppearance()
-        setupViewControllers()
-    }
-    
-    // MARK: - Setup
-    
-    private func setupViewControllers() {
-        let homeVC = makeTabItem(
-            viewController: factory.makeHomeViewController(),
+
+    // MARK: - UI
+
+    private let containerView = UIView()
+
+    private let customTabBarView = CustomTabBarView(items: [
+        .init(
             title: "홈",
             selectedImageName: "ic_home_black_24_ios",
             unselectedImageName: "ic_home_gray_24_ios"
-        )
-        
-        let vocabVC = makeTabItem(
-            viewController: factory.makeWordBookViewController(),
+        ),
+        .init(
             title: "단어장",
             selectedImageName: "ic_book_black_24_ios",
             unselectedImageName: "ic_book_gray_24_ios"
-        )
-        
-        let feedVC = makeTabItem(
-            viewController: factory.makeFeedViewController(),
+        ),
+        .init(
             title: "피드",
             selectedImageName: "ic_community_black_24_ios",
             unselectedImageName: "ic_community_gray_24_ios"
-        )
-        
-        let myVC = makeTabItem(
-            viewController: factory.makeMypageViewController(),
+        ),
+        .init(
             title: "마이",
             selectedImageName: "ic_my_black_24_ios",
             unselectedImageName: "ic_my_gray_24_ios"
         )
-        
-        viewControllers = [homeVC, vocabVC, feedVC, myVC]
+    ])
+
+    // MARK: - Init
+
+    public init(diContainer: ViewControllerFactory) {
+        self.factory = diContainer
+        super.init(nibName: nil, bundle: nil)
     }
-    
-    private func setupTabBarAppearance() {
-        let appearance = UITabBarAppearance()
-        appearance.backgroundColor = .white
-        appearance.shadowColor = .gray200
-        
-        let itemAppearance = UITabBarItemAppearance()
-        itemAppearance.normal.titleTextAttributes = [
-            .font: UIFont.pretendard(.cap_r_12),
-        ]
-        itemAppearance.selected.titleTextAttributes = [
-            .font: UIFont.pretendard(.cap_r_12),
-        ]
-        
-        appearance.stackedLayoutAppearance = itemAppearance
-        
-        tabBar.standardAppearance = appearance
-        tabBar.scrollEdgeAppearance = appearance
-        
-        tabBar.tintColor = .black
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    
-    private func makeTabItem(
-        viewController: UIViewController,
-        title: String,
-        selectedImageName: String,
-        unselectedImageName: String
-    ) -> UIViewController {
-        let nav = UINavigationController(rootViewController: viewController)
-        nav.tabBarItem = UITabBarItem(
-            title: title,
-            image: UIImage(named: unselectedImageName, in: .module, compatibleWith: nil)?
-                .withRenderingMode(.alwaysOriginal),
-            selectedImage: UIImage(named: selectedImageName, in: .module, compatibleWith: nil)?
-                .withRenderingMode(.alwaysOriginal)
-        )
+
+    // MARK: - LifeCycle
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        definesPresentationContext = true
+        setupChildViewControllers()
+        setupUI()
+        setupLayout()
+        setupTabBarAction()
+        selectTab(at: 0)
+    }
+
+    public override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        view.bringSubviewToFront(customTabBarView)
+    }
+
+    // MARK: - Setup
+
+    private func setupChildViewControllers() {
+        let homeNav = makeNavigationController(root: factory.makeHomeViewController())
+        let vocabNav = makeNavigationController(root: factory.makeWordBookViewController())
+        let feedNav = makeNavigationController(root: factory.makeFeedViewController())
+        let myNav = makeNavigationController(root: factory.makeMypageViewController())
+
+        childNavigationControllers = [homeNav, vocabNav, feedNav, myNav]
+    }
+
+    private func makeNavigationController(root: UIViewController) -> UINavigationController {
+        let nav = UINavigationController(rootViewController: root)
+        nav.delegate = self
         return nav
+    }
+
+    private func setupUI() {
+        view.addSubviews(containerView, customTabBarView)
+    }
+
+    private func setupLayout() {
+        containerView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+        customTabBarView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-customTabBarHeight)
+        }
+    }
+
+    private func setupTabBarAction() {
+        customTabBarView.onSelect = { [weak self] index in
+            guard let self else { return }
+            let previousIndex = self.currentIndex
+
+            if previousIndex == index {
+                if index == 2,
+                   let feedVC = self.childNavigationControllers[index].viewControllers.first as? FeedViewController {
+                    feedVC.handleFeedTabSelected(isReSelected: true)
+                }
+                return
+            }
+
+            self.selectTab(at: index)
+        }
+    }
+
+    // MARK: - Tab Selection
+
+    private func selectTab(at index: Int) {
+        guard index >= 0, index < childNavigationControllers.count else { return }
+
+        let previousVC = childNavigationControllers[currentIndex]
+        let newVC = childNavigationControllers[index]
+
+        previousVC.willMove(toParent: nil)
+        previousVC.view.removeFromSuperview()
+        previousVC.removeFromParent()
+
+        addChild(newVC)
+        containerView.addSubview(newVC.view)
+        newVC.view.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        newVC.didMove(toParent: self)
+
+        newVC.additionalSafeAreaInsets.bottom = customTabBarHeight
+
+        currentIndex = index
+        customTabBarView.setSelectedIndex(index)
+        updateTabBarVisibility(for: newVC)
+    }
+
+    private func updateTabBarVisibility(for navigationController: UINavigationController) {
+        let shouldHide = navigationController.topViewController?.hidesBottomBarWhenPushed ?? false
+        setTabBarHidden(shouldHide, animated: false)
+    }
+
+    private func setTabBarHidden(_ hidden: Bool, animated: Bool) {
+        guard customTabBarView.isHidden != hidden else { return }
+
+        let currentNav = childNavigationControllers[currentIndex]
+        let newInset: CGFloat = hidden ? 0 : customTabBarHeight
+
+        if animated {
+            if !hidden {
+                customTabBarView.isHidden = false
+            }
+            UIView.animate(withDuration: 0.25) {
+                self.customTabBarView.alpha = hidden ? 0 : 1
+                currentNav.additionalSafeAreaInsets.bottom = newInset
+            } completion: { _ in
+                if hidden {
+                    self.customTabBarView.isHidden = true
+                }
+            }
+        } else {
+            customTabBarView.isHidden = hidden
+            customTabBarView.alpha = hidden ? 0 : 1
+            currentNav.additionalSafeAreaInsets.bottom = newInset
+        }
     }
 }
 
-// MARK: - Extension
+// MARK: - UINavigationControllerDelegate
 
-extension TabBarViewController: UITabBarControllerDelegate {
-    
-    private static var lastSelectedIndex: Int = 0
-    
-    public func tabBarController(
-        _ tabBarController: UITabBarController,
-        didSelect viewController: UIViewController
+extension TabBarViewController: UINavigationControllerDelegate {
+
+    public func navigationController(
+        _ navigationController: UINavigationController,
+        willShow viewController: UIViewController,
+        animated: Bool
     ) {
-        if tabBarController.selectedIndex == 2,
-           let nav = viewController as? UINavigationController,
-           let feedVC = nav.viewControllers.first as? FeedViewController {
-            
-            let isReSelected = (TabBarViewController.lastSelectedIndex == 2)
-            
-            feedVC.handleFeedTabSelected(isReSelected: isReSelected)
+        let shouldHide = viewController.hidesBottomBarWhenPushed
+        setTabBarHidden(shouldHide, animated: animated)
+    }
+}
+
+// MARK: - UIViewController Extension
+
+public extension UIViewController {
+
+    var customTabBarController: TabBarViewController? {
+        var current: UIViewController? = self
+        while let viewController = current {
+            if let tabBar = viewController as? TabBarViewController {
+                return tabBar
+            }
+            current = viewController.parent
         }
-        
-        TabBarViewController.lastSelectedIndex = tabBarController.selectedIndex
+        return nil
     }
 }
